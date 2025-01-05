bb91017a0a374327c6db7e66fa72e4a9445a8702
ZZZZZZ-K <142830119+ZZZZZZ-K@users.noreply.github.com>
2025-01-05 15:52:36 +0800

    Update tetris-pc.py

--- basic/tetris-pc.py
+++ basic/tetris-pc.py
@@ -31,6 +31,131 @@ class TetrisPC:
             new_bag = bag[:i] + bag[i+1:]
             for rotation in self.get_rotations(piece):
                 for x in range(len(board[0])):
+                    new_board, final_x = self.hard_drop_with_kick(board, rotation, x, piece)
+                    if new_board is not None:
+                        if self.can_pc(new_board, new_bag, hold, depth+1):
+                            self.memo[state] = True
+                            return True
+
+        if hold:
+            for i, piece in enumerate(bag):
+                if hold != piece:
+                    new_bag = bag[:i] + [hold] + bag[i+1:]
+                    for rotation in self.get_rotations(piece):
+                        for x in range(len(board[0])):
+                            new_board, final_x = self.hard_drop_with_kick(board, rotation, x, piece)
+                            if new_board is not None:
+                                if self.can_pc(new_board, new_bag, piece, depth+1):
+                                    self.memo[state] = True
+                                    return True
+
+        self.memo[state] = False
+        return False
+
+    def is_perfect_clear(self, board):
+        return np.all(board == 0)
+
+    def get_rotations(self, piece):
+        rotations = []
+        if piece == 'I':
+            rotations = [np.array([[1, 1, 1, 1]]), np.array([[1], [1], [1], [1]])]
+        elif piece == 'O':
+            rotations = [np.array([[1, 1], [1, 1]])]
+        elif piece == 'T':
+            rotations = [np.array([[1, 1, 1], [0, 1, 0]]), np.array([[1, 0], [1, 1], [1, 0]]),
+                         np.array([[0, 1, 0], [1, 1, 1]]), np.array([[0, 1], [1, 1], [0, 1]])]
+        elif piece == 'S':
+            rotations = [np.array([[0, 1, 1], [1, 1, 0]]), np.array([[1, 0], [1, 1], [0, 1]])]
+        elif piece == 'Z':
+            rotations = [np.array([[1, 1, 0], [0, 1, 1]]), np.array([[0, 1], [1, 1], [1, 0]])]
+        elif piece == 'L':
+            rotations = [np.array([[1, 0], [1, 0], [1, 1]]), np.array([[1, 1, 1], [1, 0, 0]]),
+                         np.array([[1, 1], [0, 1], [0, 1]]), np.array([[0, 0, 1], [1, 1, 1]])]
+        elif piece == 'J':
+            rotations = [np.array([[0, 1], [0, 1], [1, 1]]), np.array([[1, 0, 0], [1, 1, 1]]),
+                         np.array([[1, 1], [1, 0], [1, 0]]), np.array([[1, 1, 1], [0, 0, 1]])]
+        return rotations
+
+    def hard_drop_with_kick(self, board, piece, x, piece_type):
+        new_board = board.copy()
+        y = 0
+        final_x = x
+        for dx, dy in self.kick_table.get(piece_type, self.kick_table['default']):
+            temp_x = x + dx
+            temp_y = y + dy
+            if temp_x >= 0 and temp_x + piece.shape[1] <= board.shape[1]:
+                while temp_y + piece.shape[0] <= new_board.shape[0]:
+                    if np.any(new_board[temp_y:temp_y + piece.shape[0], temp_x:temp_x + piece.shape[1]] + piece > 1):
+                        break
+                    temp_y += 1
+                temp_y -= 1
+                if temp_y >= 0:
+                    new_board[temp_y:temp_y + piece.shape[0], temp_x:temp_x + piece.shape[1]] += piece
+                    final_x = temp_x
+                    self.display_move_instructions(piece, final_x)
+                    return new_board, final_x
+        return None, x
+
+    def display_move_instructions(self, piece, target_column):
+        moves = target_column - self.spawn_x
+        if moves > 0:
+            print(f"Move {piece} right {moves} times, then HD (Hard Drop).")
+        elif moves < 0:
+            print(f"Move {piece} left {abs(moves)} times, then HD (Hard Drop).")
+        else:
+            print(f"HD (Hard Drop) {piece} from spawn point.")
+
+    def suggest_moves(self, board, bag, hold):
+        suggestions = []
+        for i, piece in enumerate(bag):
+            new_bag = bag[:i] + bag[i+1:]
+            for rotation in self.get_rotations(piece):
+                for x in range(len(board[0])):
+                    new_board, _ = self.hard_drop_with_kick(board, rotation, x, piece)
+                    if new_board is not None and self.can_pc(new_board, new_bag, hold):
+                        suggestions.append((piece, rotation, x, new_board))
+        return suggestions
+
+    def plot_board(self, board):
+        plt.imshow(board, cmap='gray_r')
+        plt.xticks(np.arange(-.5, 10, 1), [])
+        plt.yticks(np.arange(-.5, 20, 1), [])
+        plt.grid(True, which='both', color='black', linewidth=0.5)
+        plt.title('Tetris Board')
+        plt.show()
+import numpy as np
+import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import Canvas
+import socket
+import threading
+import pickle
+
+class TetrisPC:
+    def __init__(self):
+        self.memo = {}
+        self.kick_table = {
+            'I': [(0, 0), (-2, 0), (1, 0), (-2, -1), (1, 2)],
+            'default': [(0, 0), (-1, 0), (1, 0), (-1, 1), (1, -1)]
+        }
+        self.spawn_x = 4  # 通常的方块生成位置
+
+    def can_pc(self, board, bag, hold, depth=0):
+        state = (tuple(map(tuple, board)), tuple(bag), hold, depth)
+        
+        if state in self.memo:
+            return self.memo[state]
+        
+        if self.is_perfect_clear(board):
+            return True
+        
+        if depth == len(bag):
+            return False
+
+        for i, piece in enumerate(bag):
+            new_bag = bag[:i] + bag[i+1:]
+            for rotation in self.get_rotations(piece):
+                for x in range(len(board[0])):
                     new_board = self.hard_drop(board, rotation, x, piece)
                     if new_board is not None:
                         if self.can_pc(new_board, new_bag, hold, depth+1):
